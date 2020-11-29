c2b8f40bbbaed70f606b46b86b9b26d6166de2f4
huchong <1178869226@qq.com>
2020-11-29 17:29:11 -0500

    [ADD] alert response

--- frontend/js/album-basic-function.js
+++ frontend/js/album-basic-function.js
@@ -5,80 +5,96 @@
 
 
 function loadPhoto(json_str) {
-  var results = json_str["results"];
-  for (var i = 0; i < results.length; i++){
-    var obj = results[i];
-    var photoSrc = obj["url"];
-    var labels = obj["labels"];
-    if(photoSrc != null){
-      var newElement = 
-        "<img class='img-fluid w-100' src='" + photoSrc + "' alt='Failed to open image: " + photoSrc + "'>" 
-      $("#imageCol").prepend(newElement);
+    var results = json_str["results"];
+    for (var i = 0; i < results.length; i++) {
+        var obj = results[i];
+        var photoSrc = obj["url"];
+        var labels = obj["labels"];
+        if (photoSrc != null) {
+            var newElement =
+                "<img class='img-fluid w-100' src='" + photoSrc + "' alt='Failed to open image: " + photoSrc + "'>"
+            $("#imageCol").prepend(newElement);
+        }
     }
-  }
 }
 
 function submitForm(e) {
-  e.preventDefault();
-  $("#imageCol").empty();
-  var labels = $("#labelBox").val();
-  var data = JSON.stringify({
-    "q": labels
-  });
+    e.preventDefault();
+    $("#imageCol").empty();
+    var labels = $("#labelBox").val();
+    var data = JSON.stringify({
+        "q": labels
+    });
 
-  var params = {
-    headers: {
-      param0: 'Accept:application/json',
-    },
-  };
+    var params = {
+        headers: {
+            param0: 'Accept:application/json',
+        },
+    };
 
-  var apigClient = apigClientFactory.newClient();
-  apigClient.searchGet(params,data).then((response) => {
+    var apigClient = apigClientFactory.newClient();
+    apigClient.searchGet(params, data).then((response) => {
         console.log(response);
         loadPhoto(response);
     }).catch((error) => {
         console.log('an error occurred', error);
-        errMsg = "Failed.<br>" + error.toString();
-        alert("errMsg");
+        var errMsg = "Failed.<br>" + error.toString();
+        alert(errMsg);
 
     })
 }
 
 function upload(e) {
-  e.preventDefault();
-  var fileToLoad = document.getElementById('images').files[0];
+    e.preventDefault();
+    var fileToLoad = document.getElementById('images').files[0];
 
-  var filename = fileToLoad.name;
-  var extension = fileToLoad.type;
+    var filename = fileToLoad.name;
+    var extension = fileToLoad.type;
 
-  console.log(filename);
-  console.log(extension);
+    console.log(filename);
+    console.log(extension);
 
-  var fileReader = new FileReader();
+    var fileReader = new FileReader();
 
-  fileReader.onload = function(fileLoadedEvent){
-    var srcData = fileLoadedEvent.target.result; // base64
-    // TODO 
-    var params = {
-      headers: {
-        param0: 'Accept:application/json',
-      }
-    };
-    var solution = srcData.split("base64,")[1];
-    // console.log(solution)
-    var body = JSON.stringify({"img": solution, "name":fileToLoad.name});
+    fileReader.onload = function (fileLoadedEvent) {
+        var srcData = fileLoadedEvent.target.result; // base64
+        // TODO
+        var params = {
+            headers: {
+                param0: 'Accept:application/json',
+            }
+        };
+        var solution = srcData.split("base64,")[1];
+        // console.log(solution)
+        var body = JSON.stringify({"img": solution, "name": fileToLoad.name});
 
 
-    var apigClient = apigClientFactory.newClient();
-    apigClient.uploadPut(params,body)
-    .then(function(result){
-      console.log("uploaded")
-    }).catch(function(result){
-      console.log("fail")
-    });
+        var apigClient = apigClientFactory.newClient();
+        apigClient.uploadPut(params, body)
+            .then(function (result) {
+                console.log("uploaded");
+                uploadShowAlert("alert-success", "Upload successfully!");
+            }).catch(function (error) {
+            console.log("fail");
+            var errMsg = "Failed.<br>" + error.body;
+            uploadShowAlert("alert-danger", errMsg);
+        });
 
-  }
-  fileReader.readAsDataURL(fileToLoad);
+    }
+    fileReader.readAsDataURL(fileToLoad);
+}
+
+// type: the alert type of bootstrap.
+function uploadShowAlert(type, msg) {
+    $("#formAlert").remove();
+
+    date = new Date();
+    time = date.toLocaleTimeString();
+    newElement =
+        "<div id='formAlert' class='alert top-1 " + type + "' role='alert'>" +
+        time + "<br>" + msg +
+        "</div>";
+    $("#uploadAlertCol").append(newElement);
 }
 
 
